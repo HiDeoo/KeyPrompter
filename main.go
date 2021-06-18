46e7430b75d7876c52e682c5f3fe1b68dbde323e
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2021-06-18 13:04:18 +0200

    Add binary versionning

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 
 	"github.com/HiDeoo/KeyPrompter/cli"
@@ -10,12 +11,20 @@ import (
 	flags "github.com/jessevdk/go-flags"
 )
 
+var Version = "dev"
+
 var opts struct {
-	Config string `short:"c" long:"config" description:"Optional client configuration file" value-name:"PATH"`
-	Port   uint   `short:"p" long:"port" description:"Port used to run the web UI" required:"true" value-name:"PORT"`
+	Config  string `short:"c" long:"config" description:"Optional client configuration file" value-name:"PATH"`
+	Port    uint   `short:"p" long:"port" description:"Port used to run the web UI" required:"true" value-name:"PORT"`
+	Version func() `short:"v" long:"version" description:"Print the KeyPrompter current version."`
 }
 
 func main() {
+	opts.Version = func() {
+		fmt.Printf("KeyPrompter version %s\n", Version)
+		os.Exit(0)
+	}
+
 	_, err := flags.Parse(&opts)
 
 	if err != nil {
